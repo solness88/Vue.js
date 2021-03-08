2c18f152148832e6a6e843f4de395a245ee7974e
hirokazu kobayashi <solness.stf@gmail.com>
2021-03-08 17:00:31 +0900

    Vue.jsの要素追加機能を実装

---
+++ sample.js
@@ -0,0 +1,16 @@
+var vm = new Vue({
+  el: '#app',
+  data: {
+    students: [
+      { id: 1, name: '野呂浩良', course: '機械学習コース', acceptancePeriod: '2019年01月期' },
+      { id: 2, name: '富永修司', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' },
+      { id: 3, name: '斉藤一起', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' }
+    ],
+  },
+  methods: {
+    addStudent: function() {
+      var new_id = this.students[this.students.length - 1].id + 1;
+      this.students.push({ id: new_id, name: this.name, course: this.course, acceptancePeriod: this.acceptancePeriod });
+    }
+  }
+})
